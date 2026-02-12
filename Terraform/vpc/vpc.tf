e479f63e9c01d5169dcd6edcacf78cfa8302debc
Ram-eng01 <rammohan63025@gmail.com>
2026-02-12 10:09:29 +0530

    docker project

--- Terraform/vpc/vpc.tf
+++ Terraform/vpc/vpc.tf
@@ -72,3 +72,37 @@ resource "aws_nat_gateway" "nat" {
 
 # Resource-7: Public Route Table
 
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+    tags = merge(var.tags, { Name = "${var.environment_name}-public-rt" })
+}
+
+# Resource-8: Public Route Table Associate to Public Subnet
+
+resource "aws_route_table_association" "public_rt_assoc" {
+    for_each = aws_subnet.public
+    subnet_id = each.value.id
+    route_table_id = aws_route_table.public_rt.id
+}
+
+
+# Resource-9: Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = merge(var.tags, { Name = "${var.environment_name}-private-rt" })
+}
+
+# Resource-10: Private Route Table Association to Private Subnet
+resource "aws_route_table_association" "private_rt_assoc" {
+  for_each = aws_subnet.private
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_rt.id
+}
